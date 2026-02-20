007ea94d77cd5592341bcf035904cf74556bb63f
Sri Ragavendra .S <sriragavendra.s2004@gmail.com>
2026-02-20 08:20:05 +0530

    URLShortenerApp.java

--- URLShortenerApp.java
+++ URLShortenerApp.java
@@ -5,6 +5,7 @@ import java.util.Random;
 interface URLService {
     String shortenURL(String longUrl);
     String getLongURL(String shortCode) throws Exception;
+    double getUsageCount(String shortCode);
 }
 
 class MyURLShortener implements URLService {
